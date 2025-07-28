dc4283b60491a41b0814cc3a5922ead493198512
salmaniye <fatahillah.salman@gmail.com>
2025-07-28 19:27:27 +0700

    added more icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cicon-devs.github.io',
-  integrations: [icon()],
+  integrations: [icon({
+    lucide: () => import('@iconify-json/lucide/icons.json'),
+  })],
 
   vite: {
     plugins: [tailwindcss()]
